43ed5e6b5af66e8e1324198eb60c508000403073
kzubkov <kirya.zubkov.98@list.ru>
2023-07-01 13:01:20 +0300

    feat: add pages for routing

---
+++ code/src/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex flex-col items-center justify-center w-2/5 gap-4">
+    <FileInput />
+    <OpenPage :path="'preview'" />
+  </div>
+</template>
+
+<script>
+import OpenPage from '../components/OpenPage.vue';
+import FileInput from '../components/FileInput.vue';
+
+export default {
+  components: {
+    OpenPage,
+    FileInput,
+  },
+};
+</script>
---
+++ code/src/pages/preview.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col items-center justify-center w-2/5 gap-4">
+    <FileList />
+    <OpenPage :path="'home'" />
+  </div>
+</template>
+
+<script>
+import OpenPage from '../components/OpenPage.vue';
+import FileList from '../components/FileList.vue';
+export default {
+  components: {
+    OpenPage,
+    FileList,
+  },
+};
+</script>
